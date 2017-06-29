e0ec61c77ef4b6712bd19b448bf65a54cbfabcc6
RTVSLO\bajec_blaz <blaz.bajec@rtvslo.si>
2017-06-29 14:10:58 +0200

    first commit

---
+++ src/components/CurrentMonth.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+  <div>{{formattedDate}} </div>
+  <button @click="dec">-</button>
+  <button @click="inc">+</button>
+</div>
+</template>
+
+<script>
+export default {
+methods:{
+    dec() {
+        console.log('dec');
+
+    },
+     inc() {
+        console.log('inc');
+
+    },
+},
+  computed: {
+      formattedDate() {
+          return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM  YYYY');
+      },
+      month() {
+            return this.$store.state.currentMonth;
+        },
+       year() {
+            return this.$store.state.currentYear;
+        },
+    
+  }
+}
+</script>
