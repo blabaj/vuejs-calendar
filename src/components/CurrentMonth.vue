b71bac72d3897c4be6a0ff859ac82ef2fd6f7d81
RTVSLO\bajec_blaz <blaz.bajec@rtvslo.si>
2017-07-13 17:40:14 +0200

    second commit

--- src/components/CurrentMonth.vue
+++ src/components/CurrentMonth.vue
@@ -1,34 +1,44 @@
 <template>
-<div>
-  <div>{{formattedDate}} </div>
-  <button @click="dec">-</button>
-  <button @click="inc">+</button>
-</div>
+    <div>
+        <div>{{formattedDate}} </div>
+        <button @click="dec">-</button>
+        <button @click="inc">+</button>
+    </div>
 </template>
 
 <script>
 export default {
-methods:{
-    dec() {
-        console.log('dec');
-
+    methods: {
+        dec() {
+            if (this.month === 1) {
+                this.$store.commit('setCurrentMonth', 12)
+                this.$store.commit('setCurrentYear', this.year - 1)
+            } else {
+                this.$store.commit('setCurrentMonth', this.month - 1)
+            }
+            this.$store.commit('eventFormActive', false);
+        },
+        inc() {
+            if (this.month === 12) {
+                this.$store.commit('setCurrentMonth', 1)
+                this.$store.commit('setCurrentYear', this.year + 1)
+            } else {
+                this.$store.commit('setCurrentMonth', this.month + 1)
+            }
+            this.$store.commit('eventFormActive', false);
+        },
     },
-     inc() {
-        console.log('inc');
-
-    },
-},
-  computed: {
-      formattedDate() {
-          return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM  YYYY');
-      },
-      month() {
+    computed: {
+        formattedDate() {
+            return this.$moment(`${this.year}-${this.month}-1`, 'YYYY-M-D').format('MMMM  YYYY');
+        },
+        month() {
             return this.$store.state.currentMonth;
         },
-       year() {
+        year() {
             return this.$store.state.currentYear;
         },
-    
-  }
+
+    }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,44 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import moment from 'moment-timezone';
+moment.tz.setDefault('UTC');
 Vue.use(Vuex);
 export default new Vuex.Store({
         state: {
             currentYear: 2017,
-            currentMonth: 6
+            currentMonth: 6,
+            eventFormPosX: 0,
+             eventFormPosY: 0,
+             eventFormActive: false,
+             events: [
+                    {description: 'Random event 1', date: moment('2017-06-30', 'YYYY-MM-DD')},
+                    {description: 'Random event 1', date: moment('2017-07-01', 'YYYY-MM-DD')},
+                    {description: 'Random event 1', date: moment('2017-07-02', 'YYYY-MM-DD')},
+                ],
+            eventFormDate: moment()
+        },
+        mutations : {
+            setCurrentMonth(state, payload) {
+                state.currentMonth = payload;
+            },
+            setCurrentYear(state, payload) {
+                state.currentYear = payload;
+            },
+            eventFormPos(state, payload){
+                state.eventFormPosX = payload.x;
+                state.eventFormPosY = payload.y;
+            },
+            eventFormActive(state, payload){
+                state.eventFormActive = payload;
+            },
+            addEvent(state, payload) {
+                state.events.push({
+                    description: payload,
+                    date: state.eventFormDate
+                });
+            },
+            eventFormDate(state , payload) {
+                state.eventFormDate = payload
+            }
         }
-    
 });
